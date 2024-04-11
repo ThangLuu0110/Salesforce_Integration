a513e067745ee3c6e00f9375c337beec120a4a6e
Lưu Mạnh Thắng (VTI) <thanglm.b17vt327@stu.ptit.edu.vn>
2024-04-11 22:16:37 +0700

    update form

--- src/common/SearchTable.component.tsx
+++ src/common/SearchTable.component.tsx
@@ -13,28 +13,48 @@ export default class SearchTableComponent extends Component<SearchTableProps>{
                 <Formik
                     initialValues={{
                         playerName: '',
+                        minAge: '',
+                        maxAge: '',
                         nationality: '',
                         position: '',
-
+                        positionDetail: ''
                     }}
                     onSubmit={(values) => {
                         console.log(values)
                     }}
                 >
                     <Form>
-                        <label className="search__label name" htmlFor="playerName">Player Name: </label>
-                        <Field id="playerName" name="playerName" placeholder="Player Name"/>
-                    
-                        <label className="search__label nationality" htmlFor="nationality">Nationality: </label>
-                        <Field id="nationality" name="nationality" placeholder="Nationality"/>
-                    
-                        <label className="search__label position" htmlFor="position">Position: </label>
-                        <Field id="position" name="position" component="select">
-                            <option value="">None</option>
-                            {this.props.detailPositionList.map((position, index) => (
-                                <option key={index} value={position.Value}>{position.Label}</option>
-                            ))}
-                        </Field>
+                        <div className="search-component name">
+                            <label className="search-component__label" htmlFor="playerName">Player Name: </label>
+                            <Field id="playerName" name="playerName" placeholder="Player Name"/>
+                        </div>
+                        <div className="search-component age">
+                            <label className="search-component__label" htmlFor="playerAge">Player Age: </label>
+                            <Field id="minAge" name="minAge" placeholder="From"/> 
+                            <Field id="maxAge" name="maxAge" placeholder="To"/>
+                        </div>
+                        <div className="search-component nationality">
+                            <label className="search-component__label" htmlFor="nationality">Nationality: </label>
+                            <Field id="nationality" name="nationality" placeholder="Nationality"/>
+                        </div>
+                        <div className="search-component position">
+                            <label className="search-component__label" htmlFor="position">Position: </label>
+                            <Field id="position" name="position" component="select"> 
+                                <option value="">None</option>
+                                {this.props.detailPositionList.map((position, index) => (
+                                    <option key={index} value={position.Value}>{position.Label}</option>
+                                ))}
+                            </Field>
+                        </div>
+                        <div className="search-component positionDetail">
+                            <label className="search-component__label" htmlFor="positionDetail">Detail Position: </label>
+                            <Field id="positionDetail" name="positionDetail" component="select"> 
+                                <option value="">None</option>
+                                {this.props.detailPositionList.map((position, index) => (
+                                    <option key={index} value={position.Value}>{position.Label}</option>
+                                ))}
+                            </Field>
+                        </div>
                         <div className="search__button">
                             <button type="submit">Submit</button>
                         </div>
